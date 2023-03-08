42403c97b9cddf031258d4e12df5aa4134717acc
David <david@tangentdigitalagency.com>
2023-03-08 09:37:32 -0700

    added userid

--- src/data/postToJangle.js
+++ src/data/postToJangle.js
@@ -34,7 +34,7 @@ export const postDataToJangle = async () => {
     let data = {
     "meta": {
       "originally_created": today,
-      
+      "originally_created_by": userId,
       "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
       "landing_page_url": "https://www.commercial.insurtechgroups.com",
       "tcpa_compliant": true,

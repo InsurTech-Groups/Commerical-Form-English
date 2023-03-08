84b994649abaac39e3270cf5d631a569bb18db84
David <david@tangentdigitalagency.com>
2023-03-08 09:19:41 -0700

    removed vercel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginReact from '@bugsnag/plugin-react'
-import { inject } from '@vercel/analytics';
 
 
 //TODO: Add Bugsnag
@@ -13,7 +12,7 @@ import { inject } from '@vercel/analytics';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-inject();
+
 
 // Bugsnag.start({
 //   apiKey: '86cc05e770db4d552eaabcc2a6228b41',

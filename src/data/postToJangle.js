9f49e99941103d858e19e7af20fc395fa9012f20
David <david@tangentdigitalagency.com>
2023-03-10 10:15:42 -0700

    updated posting into zapier account

--- src/data/postToJangle.js
+++ src/data/postToJangle.js
@@ -1,5 +1,6 @@
 import { userData } from "./userData";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 export const postDataToJangle = async () => {
 
@@ -38,7 +39,8 @@ export const postDataToJangle = async () => {
       "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
       "landing_page_url": "https://www.commercial.insurtechgroups.com",
       "tcpa_compliant": true,
-      'tcpa_consent_text': "By clicking Get My Free Quote below, I am agreeing to receive text messages from InsurTech Groups and business partners. I provide my signature expressly consenting to recurring contact from Insurtech Groups or its business partners at the number I provided regarding products or services via live, automated or prerecorded telephone call, text message, or email. I understand that my telephone company may impose charges on me for these contacts, and I am not required to enter into this agreement as a condition of purchasing property, goods, or services. I understand that I can revoke this consent at any time. Terms & conditions & Privacy policy apply."
+        'tcpa_consent_text': "By clicking Get My Free Quote below, I am agreeing to receive text messages from InsurTech Groups and business partners. I provide my signature expressly consenting to recurring contact from Insurtech Groups or its business partners at the number I provided regarding products or services via live, automated or prerecorded telephone call, text message, or email. I understand that my telephone company may impose charges on me for these contacts, and I am not required to enter into this agreement as a condition of purchasing property, goods, or services. I understand that I can revoke this consent at any time. Terms & conditions & Privacy policy apply.",
+      'trusted_form_url': ''
       },
 
       "contact": {
@@ -71,23 +73,40 @@ export const postDataToJangle = async () => {
   }
 
 
-  fetch('https://api.jangl.com/v2/general/capture', {
+  fetch('https://hooks.zapier.com/hooks/catch/13124392/3bsi2io/', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Token aa87c88ca907b5a881131aa4e985542765c45d1f'
-    },
-    body: JSON.stringify(data)
+  
+    body: data
   })
     .then(response => response.json())
     .then(data => {
       toast.success('Form Submitted');
-      console.log(data.json())
+      console.log(data)
     })
     .catch((error) => {
       toast.error('Error in submitting your form!')
       return
     });
   
+
+
+  // axios.post('https://leads.ricochet.me/api/v1/lead/create/COMM-Commercial-Internal-Form/?token=ea527c772f0fe84238e916ff02f32ae8&cid=General', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Origin': '*'
+  //   },
+  //   body: data
+  // })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     toast.success('Form Submitted');
+  //     console.log(data)
+  //   })
+  //   .catch((error) => {
+  //     toast.error('Error in submitting your form!')
+  //     return
+  //   });
+
 
 }
